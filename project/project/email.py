ad7bc04083041bf1356b978c53f52cc996a13336
Aloysius Thomas <aloysiusthomas@xeoscript.com>
2021-02-02 16:52:12 +0530

    base template email

---
+++ project/project/email.py
@@ -0,0 +1,7 @@
+from django.conf import settings
+from django.core.mail import send_mail
+
+
+def send_email(subject, message, recipient_list):
+    email_from = settings.EMAIL_HOST_USER
+    send_mail(subject, message, email_from, recipient_list, fail_silently=True)
--- project/project/urls.py
+++ project/project/urls.py
@@ -3,8 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from accounts.views import home_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
