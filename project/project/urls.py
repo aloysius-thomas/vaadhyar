fd57db0f0b1de1140f103ad9a1743f0580ca25fe
Aloysius Thomas <aloysiusthomas@xeoscript.com>
2021-02-05 00:51:51 +0530

    change password

--- project/project/urls.py
+++ project/project/urls.py
@@ -7,23 +7,43 @@ from django.urls import path
 
 from accounts.views import about_us
 from accounts.views import change_password
-from accounts.views import home_view
 from accounts.views import dashboard_view
+from accounts.views import home_view
 from accounts.views import login_view
 from accounts.views import logout_view
 from accounts.views import online_courses
-from institute.views import viewteacher, nxcad, nxnas, nxcam, revitmep, prime, autocadec, ecrevit, ecprim, java, web, \
-    tu, python, autocad, revit, googles, stadd, etab, vray, revitst, navis, prim
-from institute.views import courseslist
-from institute.views import civil
-from institute.views import cs
-from institute.views import mech
-from institute.views import ec
+from institute.views import ansys
+from institute.views import autocad
+from institute.views import autocadec
 from institute.views import autocadmech
 from institute.views import catia
-from institute.views import solid
-from institute.views import ansys
+from institute.views import civil
+from institute.views import courseslist
+from institute.views import cs
 from institute.views import ds
+from institute.views import ec
+from institute.views import ecprim
+from institute.views import ecrevit
+from institute.views import etab
+from institute.views import googles
+from institute.views import java
+from institute.views import mech
+from institute.views import navis
+from institute.views import nxcad
+from institute.views import nxcam
+from institute.views import nxnas
+from institute.views import prim
+from institute.views import prime
+from institute.views import python
+from institute.views import revit
+from institute.views import revitmep
+from institute.views import revitst
+from institute.views import solid
+from institute.views import stadd
+from institute.views import tu
+from institute.views import viewteacher
+from institute.views import vray
+from institute.views import web
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,7 +53,6 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('about-us/', about_us, name='about-us'),
     path('online-courses/', online_courses, name='online-courses'),
-
 
     path('change-password/', change_password, name='change-password'),
     path('accounts/', include('accounts.urls')),
@@ -70,7 +89,6 @@ urlpatterns = [
     url(r'^web$', web, name='web'),
     url(r'^python$', python, name='python'),
     url(r'^tu$', tu, name='tu'),
-
 
 ]
 
